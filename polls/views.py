e8aba3d0b65193e0d3ceb716a2dc077d8c658144
eirikjg46 <eirikjg@hiof.no>
2023-10-26 14:08:57 +0200

    nyeste

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,40 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+import datetime
+# import the logging library
+import logging
+
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import NameForm
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 from .models import Question, Choice
+
+kids = False
+
+
+@csrf_exempt
+def test(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, "name.html", {"form": form})
 
 
 class IndexView(generic.ListView):
@@ -11,12 +43,18 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        kids = True
-        activity = True
+        filters = {}
 
+        if kids:
+            filters['for_kids'] = True
+
+        activity = False
+
+        if activity:
+            filters['high_activity'] = True
 
         """Return the last five published questions."""
-        return Question.objects.filter(for_kids=kids, high_activity=activity).order_by("pub_date")[:]
+        return Question.objects.filter(**filters).order_by("pub_date")[:]
 
 
 class DetailView(generic.DetailView):
